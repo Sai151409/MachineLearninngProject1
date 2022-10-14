b8408d1b40a0ffab38b5d1e70779670a8e270631
srisairam naidu <sairamnaidu694@gmail.com>
2022-10-14 18:42:23 +0530

    updated 11

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return 'Staring My First Machine Learning Project'
+    try:
+        raise Exception('We are testing custom exception')
+    except Exception as e:
+        housing = HousingException(e, sys)
+        logging.info(housing.error_message)
+        logging.info('Sucessfully app is running')
+    return 'This is my first Machine Learning Project'
+    
 
 
 if __name__ == "__main__" : 
---
+++ housing/exception/__init__.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+
+class HousingException(Exception):
+    
+    def __init__(self, error_message:Exception, error_details:sys):
+        super().__init__(error_message)
+        self.error_message = HousingException.get_detailed_error_message(error_message=error_message,
+                                                                         error_details=error_details)
+        
+    
+    @staticmethod
+    def get_detailed_error_message(error_message:Exception, error_details:sys)->str:
+        
+        """
+        error_message : Exception object
+        error_detail : object of sys module
+        """
+        
+        _, _, exec_tab = error_details.exc_info()
+        line_number = exec_tab.tb_frame.f_lineno
+        file_name = exec_tab.tb_frame.f_code.co_filename
+        error_message =  f'Error has occured in script : [{file_name}], at line number : [{line_number}], error message : [{error_message}]'
+        return error_message
+    
+    def __str__(self) -> str:
+        return self.error_message
+    
+    def __repr__(self) -> str:
+        return HousingException.__name__.str()
+    
