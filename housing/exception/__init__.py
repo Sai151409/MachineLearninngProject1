9a11bac50dc8b66e02dc15fa588458ffdc6ad8df
srisairam naidu <sairamnaidu694@gmail.com>
2022-10-21 19:22:45 +0530

    up to date

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -19,9 +19,10 @@ class HousingException(Exception):
         """
         
         _, _, exec_tab = error_details.exc_info()
-        line_number = exec_tab.tb_lineno
+        try_block_number = exec_tab.tb_lineno
+        except_block_number = exec_tab.tb_frame.f_lineno
         file_name = exec_tab.tb_frame.f_code.co_filename
-        error_message =  f'Error has occured in script : [{file_name}], at line number : [{line_number}], error message : [{error_message}]'
+        error_message =  f'Error has occured in script : [{file_name}], at try block line number : [{try_block_number}] and except block line number : [{except_block_number}]  error message : [{error_message}]'
         return error_message
     
     def __str__(self) -> str:
