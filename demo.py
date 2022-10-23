69875f752174710298590f1998f888ba1816e25f
srisairam naidu <sairamnaidu694@gmail.com>
2022-10-23 18:19:57 +0530

    updated456

--- demo.py
+++ demo.py
@@ -1,13 +1,16 @@
 from housing.pipeline.pipeline import Pipeline
 from housing.logger import logging
 from housing.config.configuration import Configuration
+from housing.component.data_ingestion import DataIngestion
+from housing.component.data_validation import DataValidation
+from housing.config.configuration import Configuration
 
 def main():
     try:
-        # pipeline = Pipeline()
-        # pipeline.run_pipeline()
-        data_validation_config = Configuration().get_data_validation_config()
-        print(data_validation_config)
+        data_ingestion_artifact = DataIngestion(data_ingestion_config=Configuration().get_data_ingestion_config())
+        data_validation = DataValidation(data_validation_config=Configuration().get_data_validation_config(),
+                                         data_ingestion_artifact=data_ingestion_artifact.intitate_data_ingestion())
+        print(data_validation.validate_dataset_schema())
     except Exception as e:
         logging.error(f"{e}")
         print(e)
--- housing/component/data_validation.py
+++ housing/component/data_validation.py
@@ -1,8 +1,12 @@
 import os, sys
+
+from matplotlib import test
 from housing.entity.config_entity import DataValidationConfig
 from housing.entity.artifact_entity import DataIngestionArtifact
 from housing.logger import logging
 from housing.exception import HousingException
+import pandas as pd
+from housing.util.util import read_yaml_file
 
 
 class DataValidation:
@@ -48,7 +52,28 @@ class DataValidation:
     def validate_dataset_schema(self) -> bool:
         try:
             validation_status = False
-            validation_status = True
+            train_df = pd.read_csv(self.data_ingestion_artifact.train_file_path)
+            test_df = pd.read_csv(self.data_ingestion_artifact.test_file_path)
+            schema_file = read_yaml_file(self.data_validation_config.schema_file_path)
+            logging.info('Validating the train dataset, test dataset and schema file')
+            if len(list(train_df.columns)) == len(list(test_df.columns)) == len(list(schema_file['columns'])):
+                for i in schema_file['domain_value']:
+                    if list(train_df[i].unique()).sort() == list(test_df[i].unique()).sort() == schema_file['domain_value'][i].sort():
+                        if list(train_df.columns) == list(test_df.columns) == list(schema_file['columns']):
+                            validation_status = True
+                        else:
+                            message = f"""Columns are not same in train dataset : {train_df}, test dataset :{test_df}, 
+                            schemafile :{schema_file} that's why we don't take this dataset."""
+                            raise Exception(message)
+                    else:
+                        message = f"""No fo columns are different in train dataset : {train_df}, test dataset :{test_df}, 
+                        schemafile :{schema_file} that's why we don't take this dataset."""
+                        raise Exception(message)
+            else:
+                message = f"""Columns are not same in train dataset : {train_df}, test dataset :{test_df}, 
+                schemafile :{schema_file} that's why we don't take this dataset."""
+                raise Exception(message)
+            logging.info("Validation of train_df, test_df and schema file successfully completed.")
             return validation_status
         except Exception as e:
             raise HousingException(e, sys) from e    
